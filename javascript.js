48141511d724442fcc1421f3172635b15d21f3a3
Dorcas Chagas <dorcaspereira30@gmail.com>
2023-04-19 00:32:26 -0300

    02

---
+++ javascript.js
@@ -0,0 +1,35 @@
+const botao = document.getElementById('bntclique'); // seleciona o botão pelo ID
+
+botao.addEventListener('click', fnBusca)
+
+
+
+
+
+
+
+
+
+// 
+//     window.getElementById(HTMLAllCollection);
+//   // aqui você poderá executar sua função de busca de itens da outra página HTML
+// });
+
+// fetch('index02.html')
+//   .then(response => response.text())
+//   .then(data => {
+//     const doc = new DOMParser().parseFromString(data, 'text/index02.html'); // cria um objeto Document a partir do conteúdo da página HTML carregada
+//     const itens = doc.querySelectorAll('.meusItens'); // seleciona os elementos com a classe 'meusItens' na página HTML carregada
+//     // faça algo com os itens selecionados
+//   });
+
+//   fetch('index02.html')
+//   .then(response => response.text())
+//   .then(data => {
+//     const doc = new DOMParser().parseFromString(data, 'text/index02.html');
+//     const itens = doc.querySelectorAll('.cat');
+//     const container = document.getElementById('categorias'); // seleciona o elemento que receberá os itens
+//     for (let item of itens) {
+//       container.appendChild(index02.HTMLAllCollection); // adiciona cada item ao container
+//     }
+//   });
